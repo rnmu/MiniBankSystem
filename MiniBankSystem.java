facdbabd3e4edacd82f6cd526c3647c381560d8e
rayan <rayanalhashmi8@gmail.com>
2025-07-21 23:28:57 +0400

    Created and tested two BankAccount objects

--- MiniBankSystem.java
+++ MiniBankSystem.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class MiniBankSystem {
 
     public static void main(String[] args) {
+
+        // Create two BankAccount objects
+        BankAccount account1 = new BankAccount("Liam Carter", "ACC123", 100.0);
+        BankAccount account2 = new BankAccount("Alex Smith", "ACC456", 250.0);
+
+        // Display account details
+        System.out.println("Account 1");
+        account1.showAccountInfo();
+
+        System.out.println("Account 2");
+        account2.showAccountInfo();
+
         Scanner inputReader = new Scanner(System.in);
         boolean running = true;
 
